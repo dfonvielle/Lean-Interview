7203f9c5649e8ec7b7cf00f69293e620b0f7a5e6
David Fonvielle <dfonvielle@hotmail.com>
2012-02-02 16:02:02 -0600

    Created new rails app with an .rvmrc file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeanInterview::Application.config.secret_token = '3d711a9be54385cd91295cc4a7cfb18039b9dd40467a91a64ad7568bd1a7f207076290c99d68d76f1b5c7c1fc844a76013dcd99fe5a87ab389e9b79659bea6c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LeanInterview::Application.config.session_store :cookie_store, key: '_lean_interview_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LeanInterview::Application.config.session_store :active_record_store
